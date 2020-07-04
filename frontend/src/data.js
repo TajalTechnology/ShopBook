8175a23c3d571c5793ecf6d41e3ed2988f7cabc0
unknown <TajalTechnology>
2020-07-04 20:14:25 +0600

    frontEnd Complete

---
+++ frontend/src/data.js
@@ -0,0 +1,84 @@
+export default{
+    products:[
+        {
+        _id: 1,
+        name:'fit shirt',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'tajal',
+        rating:4.5,
+        numReviews:10
+
+        },
+        {
+            _id: 2,
+            name:'slim shirt',
+            image:'/images/d2.jpg',
+            price:70,
+            brand:'tajal',
+            rating:4.5,
+            numReviews:10
+    
+        },
+        {
+            _id: 3,
+            name:'pant shirt',
+            image:'/images/d1.jpg',
+            price:60,
+            brand:'tajal',
+            rating:4.5,
+            numReviews:10
+    
+        },
+        {
+            _id: 4,
+            name:'pant shirt',
+            image:'/images/d1.jpg',
+            price:60,
+            brand:'tajal',
+            rating:4.5,
+            numReviews:10
+    
+        },
+        {
+            _id: 5,
+            name:'slim shirt',
+            image:'/images/d1.jpg',
+            price:60,
+            brand:'tajal',
+            rating:4.5,
+            numReviews:10
+    
+            },
+            {
+                _id: 6,
+                name:'fit shirt',
+                image:'/images/d1.jpg',
+                price:70,
+                brand:'tajal',
+                rating:4.5,
+                numReviews:10
+        
+            },
+            {
+                _id: 7,
+                name:'pant shirt',
+                image:'/images/d1.jpg',
+                price:60,
+                brand:'tajal',
+                rating:4.5,
+                numReviews:10
+        
+            },
+            {
+                _id: 8,
+                name:'pant shirt',
+                image:'/images/d1.jpg',
+                price:60,
+                brand:'tajal',
+                rating:4.5,
+                numReviews:10
+        
+            }
+]
+}
